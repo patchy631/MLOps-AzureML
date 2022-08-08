34d40f0efea34dc14a817b9600ca9220dfe4ff89
patchy631 <arsingh1131@gmail.com>
2022-08-08 15:21:04 +0530

    Adding Atrifacts for MLOps demo project

---
+++ environment_setup/install-requirements.sh
@@ -0,0 +1,7 @@
+python --version
+#pip install --upgrade azure-cli
+pip install azure-cli==2.20.0
+pip install --upgrade azureml-sdk[cli]
+pip install pytest
+pip install pytest-cov
+#pip install -r requirements.txt
